7d619c52558f0007cbd92fb717a20c53d22c2318
Cigdem Turkmen <cigdemturkmen57@outlook.com>
2024-08-03 19:42:22 +0300

    nft image is uploaded

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,4 +1,4 @@
-import wallet from "../wba-wallet.json"
+import wallet from "../wba-dev-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
@@ -16,15 +16,25 @@ umi.use(signerIdentity(signer));
 (async () => {
     try {
         //1. Load image
+        const image = await readFile("C:/Users/cigde/source/repos/solana-starter/ts/assets/generug.png" ); // buffer
+
         //2. Convert image to generic file.
+        const genericFile = createGenericFile(image, "generug", { contentType: "image/png"});
+
         //3. Upload image
+        const [myUri] = await umi.uploader.upload([genericFile]);
 
-        // const image = ???
-
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
+        console.log("Your image URI: ", myUri);
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
     }
 })();
+
+// region: Note to Self
+/*
+Here we are uploading our nft image to Arweave by using irysUploader.
+
+The result Uri of my image is :  https://arweave.net/8fkkkkf-w86OqEYKIxKtuqy95_UKZQi7skLpILHwhvo
+*/
+// endregion: Note to Self
